f777be8aebb9abef4d034050cf7631ff9658b6f3
Jon Andersson <andersson.jon@gmail.com>
2019-12-30 19:29:08 +0100

    Add more basic functions for GitHub, but not used yet.

--- routes/hearsay-admin.js
+++ routes/hearsay-admin.js
@@ -3,7 +3,6 @@ const btoa = require('btoa-lite');
 const { add, get, list } = require('../lib/github')('j0n', 'hearsay.se');
 
 routes.get('/list', async (req, res) => {
-  console.log('get List');
   try {
     const files = await list(`/data`);
     res.send(files.data);
@@ -16,22 +15,21 @@ routes.get(`/:file`, async (req, res) => {
   const { file } = req.params;
   try {
     const content = await get(`data/${file}.json`)
-    res.json({content});
+    res.json({data: content});
   } catch (err) {
     res.json({content: ''});
   }
 })
 
 routes.post(`/:file`, async (req, res) => {
-  console.log('POOOST');
   const { file } = req.params;
-  const { content = '' } = req.body;
+  const { content = '', sha = false } = req.body;
   try {
     await add(`data/${file}.json`, `upsert ${file}`, btoa(content));
     res.send('done');
   } catch(err) {
     console.log(err);
-    res.send(err);
+    res.status(500);
   }
 })
 
