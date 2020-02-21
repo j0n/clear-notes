89817f641f022cb60c43c3b3f6d816c64037df0b
Jon Andersson <andersson.jon@gmail.com>
2020-02-21 12:15:08 +0100

    A bit modern lint.
    Stripe non chars from filename.

--- index.js
+++ index.js
@@ -1,70 +1,72 @@
 require('dotenv').config()
-const btoa = require('btoa-lite');
-const express = require('express');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const bodyParser = require('body-parser');
+const btoa = require('btoa-lite')
+const express = require('express')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const bodyParser = require('body-parser')
 
-const additionalRepo = require('./routes/additional-repo');
-const { sign, isAuthorized } = require('./lib/auth');
-const github = require('./lib/github');
+const additionalRepo = require('./routes/additional-repo')
+const { sign, isAuthorized } = require('./lib/auth')
+const github = require('./lib/github')
 
-const { GITHUB_PATH = 'note', GITHUB_REPO, GITHUB_USER } = process.env;
-const { add, get, list } = github(GITHUB_USER, GITHUB_REPO);
+const { GITHUB_PATH = 'note', GITHUB_REPO, GITHUB_USER } = process.env
+const { add, get, list } = github(GITHUB_USER, GITHUB_REPO)
 
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.json())
 app.use(cors())
 app.use(fileUpload({
-    limits: { fileSize: 50 * 1024 * 1024 },
-}));
+  limits: { fileSize: 50 * 1024 * 1024 },
+}))
 app.use(express.static('static'))
 app.use(['/login', '/n/*'], express.static('static'))
 app.use('/h', express.static(__dirname + '/h'))
 
 app.post('/image', isAuthorized, async (req, res) => {
-  const { file } = req.files;
-  await add(`image/${file.name}`, `Uploading ${file.name}`, file.data.toString('base64'));
+  const { file } = req.files
+  await add(`image/${file.name}`, `Uploading ${file.name}`, file.data.toString('base64'))
+  res.send('ok')
 })
 
 app.get(`/${GITHUB_PATH}/list`, isAuthorized, async (req, res) => {
   try {
-    const files = await list(`/${GITHUB_PATH}`);
-    res.send(files.data);
+    const files = await list(`/${GITHUB_PATH}`)
+    res.send(files.data)
   } catch(err) {
-    res.send(err);
+    res.send(err)
   }
 })
 app.get(`/${GITHUB_PATH}/:file`, isAuthorized, async (req, res) => {
-  const { file } = req.params;
+  const { file } = req.params
   try {
     const content = await get(`${GITHUB_PATH}/${file}`)
-    res.json({content});
+    res.json({content})
   } catch (err) {
-    res.json({content: ''});
+    res.json({content: ''})
   }
 })
 
-app.post(`/auth/login`, (req, res) => {
-  const { password = '', email = '' } = req.body;
+app.post('/auth/login', (req, res) => {
+  const { password = '', email = '' } = req.body
   if (password === process.env.PASSWORD && email === process.env.EMAIL) {
-    return res.json({ token: sign(email) });
+    return res.json({ token: sign(email) })
   }
   res.status(401).json({
-    error: 'Wrong credentials'
-  });
-});
+    error: 'Wrong credentials',
+  })
+})
 app.post(`/${GITHUB_PATH}/:file`, isAuthorized, async (req, res) => {
-  const { file } = req.params;
-  const { content = '' } = req.body;
+  const { file } = req.params
+  const { content = '' } = req.body
+  const fileName = file.replace(/[\W_]+/g,' ')
   try {
-    await add(`${GITHUB_PATH}/${file}`, `upsert ${file}`, btoa(content));
-    res.send('done');
+    await add(`${GITHUB_PATH}/${fileName}`, `upsert ${fileName}`, btoa(content))
+    res.send('done')
   } catch(err) {
-    res.send(err);
+    res.send(err)
   }
-});
+})
 
 
-app.use('/hearsay', isAuthorized, additionalRepo);
+app.use('/hearsay', isAuthorized, additionalRepo)
 app.listen(process.env.PORT || 7764)
