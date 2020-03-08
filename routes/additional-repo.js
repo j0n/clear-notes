4e80bc0cd9c5c130a889bf660b1a6a835c67a8da
Jon Andersson <andersson.jon@gmail.com>
2020-03-08 21:40:34 +0100

    lint fixes

--- routes/additional-repo.js
+++ routes/additional-repo.js
@@ -1,39 +1,38 @@
-const express = require('express');
-const routes = require('express').Router();
-const btoa = require('btoa-lite');
-const { GITHUB_ADDITIONAL_REPO, GITHUB_USER } = process.env;
-const github = require('../lib/github');
-const { add, get, list } = github(GITHUB_USER, GITHUB_ADDITIONAL_REPO);
+const routes = require('express').Router()
+const btoa = require('btoa-lite')
+const { GITHUB_ADDITIONAL_REPO, GITHUB_USER } = process.env
+const github = require('../lib/github')
+const { add, get, list } = github(GITHUB_USER, GITHUB_ADDITIONAL_REPO)
 
 routes.get('/list', async (req, res) => {
   try {
-    const files = await list(`/data`);
-    res.send(files.data);
+    const files = await list('/data')
+    res.send(files.data)
   } catch(err) {
-    res.send(err);
+    res.send(err)
   }
 })
 
-routes.get(`/:file`, async (req, res) => {
-  const { file } = req.params;
+routes.get('/:file', async (req, res) => {
+  const { file } = req.params
   try {
     const content = await get(`data/${file}.json`)
-    res.json({data: content});
+    res.json({data: content})
   } catch (err) {
-    res.json({content: ''});
+    res.json({content: ''})
   }
 })
 
-routes.post(`/:file`, async (req, res) => {
-  const { file } = req.params;
-  const { content = '', sha = false } = req.body;
+routes.post('/:file', async (req, res) => {
+  const { file } = req.params
+  const { content = '' } = req.body
   try {
-    await add(`data/${file}.json`, `upsert ${file}`, btoa(content));
-    res.send('done');
+    await add(`data/${file}.json`, `upsert ${file}`, btoa(content))
+    res.send('done')
   } catch(err) {
-    console.log(err);
-    res.status(500);
+    console.log(err)
+    res.status(500)
   }
 })
 
-module.exports = routes;
+module.exports = routes
