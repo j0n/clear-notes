0cbd329dade8892751d63d29e6bae9dfb6532009
Jon Andersson <andersson.jon@gmail.com>
2020-04-29 14:27:42 +0200

    Update path to string because of problems with heroku decoding

--- routes/additional-repo.js
+++ routes/additional-repo.js
@@ -2,16 +2,15 @@ const routes = require('express').Router()
 const btoa = require('btoa-lite')
 const {
   GITHUB_ADDITIONAL_REPO,
-  GITHUB_ADDITIONAL_REPO_PATH = '/data',
   GITHUB_USER,
 } = process.env
-console.log({GITHUB_ADDITIONAL_REPO_PATH})
+// const BASE_PATH = '/data/posts' // decodeURIComponent(GITHUB_ADDITIONAL_REPO_PATH)
 const github = require('../lib/github')
 const { add, get, list } = github(GITHUB_USER, GITHUB_ADDITIONAL_REPO)
 
 routes.get('/list', async (req, res) => {
   try {
-    const files = await list(GITHUB_ADDITIONAL_REPO_PATH)
+    const files = await list('data/posts')
     res.send(files.data)
   } catch(err) {
     res.send(err)
@@ -21,7 +20,7 @@ routes.get('/list', async (req, res) => {
 routes.get('/:file', async (req, res) => {
   const { file } = req.params
   try {
-    const content = await get(`${GITHUB_ADDITIONAL_REPO_PATH}/${file}.json`)
+    const content = await get(`data/posts/${file}.json`)
     res.json({data: content})
   } catch (err) {
     res.json({content: ''})
@@ -32,7 +31,7 @@ routes.post('/:file', async (req, res) => {
   const { file } = req.params
   const { content = '' } = req.body
   try {
-    await add(`${GITHUB_ADDITIONAL_REPO_PATH}/${file}.json`, `upsert ${file}`, btoa(content))
+    await add(`data/posts/${file}.json`, `upsert ${file}`, btoa(content))
     res.send('done')
   } catch(err) {
     console.log(err)
