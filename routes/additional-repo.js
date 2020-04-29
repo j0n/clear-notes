b91f81b0714b35eb9b0e2dc5980c3b63824dad01
Jon Andersson <andersson.jon@gmail.com>
2020-04-29 13:55:07 +0200

    Add possibility to change path

--- routes/additional-repo.js
+++ routes/additional-repo.js
@@ -1,12 +1,16 @@
 const routes = require('express').Router()
 const btoa = require('btoa-lite')
-const { GITHUB_ADDITIONAL_REPO, GITHUB_USER } = process.env
+const {
+  GITHUB_ADDITIONAL_REPO,
+  GITHUB_ADDITIONAL_REPO_PATH = '/data',
+  GITHUB_USER,
+} = process.env
 const github = require('../lib/github')
 const { add, get, list } = github(GITHUB_USER, GITHUB_ADDITIONAL_REPO)
 
 routes.get('/list', async (req, res) => {
   try {
-    const files = await list('/data')
+    const files = await list(GITHUB_ADDITIONAL_REPO_PATH)
     res.send(files.data)
   } catch(err) {
     res.send(err)
@@ -16,7 +20,7 @@ routes.get('/list', async (req, res) => {
 routes.get('/:file', async (req, res) => {
   const { file } = req.params
   try {
-    const content = await get(`data/${file}.json`)
+    const content = await get(`${GITHUB_ADDITIONAL_REPO_PATH}/${file}.json`)
     res.json({data: content})
   } catch (err) {
     res.json({content: ''})
@@ -27,7 +31,7 @@ routes.post('/:file', async (req, res) => {
   const { file } = req.params
   const { content = '' } = req.body
   try {
-    await add(`data/${file}.json`, `upsert ${file}`, btoa(content))
+    await add(`${GITHUB_ADDITIONAL_REPO_PATH}/${file}.json`, `upsert ${file}`, btoa(content))
     res.send('done')
   } catch(err) {
     console.log(err)
