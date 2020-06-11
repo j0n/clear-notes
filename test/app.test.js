3f5d5aeaa3d2579b0e085e947283270fb525d1a7
Jon Andersson <andersson.jon@gmail.com>
2020-06-11 13:32:25 +0200

    Update tests

--- test/app.test.js
+++ test/app.test.js
@@ -5,7 +5,7 @@ afterAll(() => {
   server.close()
 })
 describe('Post Endpoints', () => {
-  it('should create a new post', async () => {
+  it('should NOT login', async () => {
     const res = await request(app)
       .post('/auth/login')
       .send({
@@ -13,5 +13,16 @@ describe('Post Endpoints', () => {
         email: 'lol@lol.com',
       })
     expect(res.statusCode).toEqual(401)
+  })
+  it('should login', async () => {
+    const { EMAIL, PASSWORD } = process.env
+    const res = await request(app)
+      .post('/auth/login')
+      .send({
+        password: PASSWORD,
+        email: EMAIL
+      })
+    expect(res.body.token).not.toBe(undefined)
+    expect(res.statusCode).toEqual(200)
   })
 })
---
+++ test/auth.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest')
+const { sign, verify } = require('../lib/auth')
+const email = 'email@example.com'
+let token = ''
+
+describe('AUTH', () => {
+  it('should create a new token', () => {
+    token = sign(email)
+    expect(token).not.toBe('')
+  })
+  it('should be verified token', () => {
+    const isVerified = verify(token, email)
+    expect(isVerified).toEqual(true)
+  })
+})
+
