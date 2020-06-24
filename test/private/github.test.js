701494310b8ffc481acdadbec34310fe98b43f09
Jon Andersson <andersson.jon@gmail.com>
2020-06-24 10:19:37 +0200

    fix update and add test for it.

--- test/private/github.test.js
+++ test/private/github.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest')
+const btoa = require('btoa-lite')
 const {
   GITHUB_REPO,
   GITHUB_USER,
@@ -6,22 +7,32 @@ const {
 
 const github = require('../../lib/github')
 if (GITHUB_USER) {
-  const { list, get } = github(GITHUB_USER, GITHUB_REPO)
+  const { list, get, add } = github(GITHUB_USER, GITHUB_REPO)
 
   describe('Github', () => {
     it('list', async () => {
-      const files = await list('data')
+      const files = await list('data/posts')
       expect(files).toHaveProperty('data')
       const { data } = files
       const posts = await list(data[0].path)
       expect(posts).toHaveProperty('data')
-      console.log(posts.data)
     })
     it('get', async () => {
       const content = await get('data/posts/001.json')
       expect(content).not.toBe(null)
       expect(JSON.parse(content)).toHaveProperty('date')
     })
+    it('update', async () => {
+      const content = '{"data":"{"content":"","title":"Hello","published":false,"slug":"036-teest","key":"036-teest","id":"036-teest"}"}'
+      try {
+        const response = await add('data/posts/036-teest.json', 'Test update', btoa(content))
+        expect(response).not.toBe(null)
+      } catch (err) {
+        expect(err).toBe(null)
+      }
+
+    })
+
   })
 
 } else {
